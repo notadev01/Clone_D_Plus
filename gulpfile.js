987f054c5912084a0b8aa6db85897b9940376061
notadev01 <notadev01@email.com>
2024-04-19 04:03:36 -0300

    Consertar processo de build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+const replace = require('gulp-replace');
 
 function minifyJs() {
     return gulp.src('./src/scripts/*.js')
@@ -11,6 +13,7 @@ function minifyJs() {
 
 function compileSass() {
     return gulp.src('./src/styles/main.scss')
+        .pipe(replace('../../assets', '../assets'))
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
@@ -19,12 +22,36 @@ function compileSass() {
         .pipe(gulp.dest('./dist/styles'))
 }
 
+function compressImages() {
+    return gulp.src('./src/images/**/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+function replaceHTML() {
+    return gulp.src('./index.html')
+        .pipe(replace('dist/', './'))
+        .pipe(replace('assets/images', './images'))
+        .pipe(gulp.dest('dist/'))
+}
+
+function copyFonts() {
+    return gulp.src('./assets/fonts/*.woff2')
+        .pipe(gulp.dest('./dist/assets/fonts'))
+}
+
 exports.sass = compileSass;
 exports.minify = minifyJs;
+exports.compress = compressImages;
+exports.replace = replaceHTML;
+exports.copyFonts = copyFonts;
 
-exports.default = gulp.parallel(compileSass, minifyJs);
+exports.default = gulp.parallel(compileSass, minifyJs, compressImages, copyFonts, replaceHTML);
 
-exports.watch = function() {
+exports.watch = function () {
     gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.series(compileSass));
     gulp.watch('./src/scripts/*.js', { ignoreInitial: false }, gulp.series(minifyJs));
+    gulp.watch('./assets/images/**/*', { ignoreInitial: false }, gulp.series(compressImages));
+    gulp.watch('./assets/fonts/*', { ignoreInitial: false }, gulp.series(copyFonts));
+    gulp.watch('./index.html', { ignoreInitial: false }, gulp.series(replaceHTML));
 }
